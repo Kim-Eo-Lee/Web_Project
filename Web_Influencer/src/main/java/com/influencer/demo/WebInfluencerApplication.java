6d4f08c4cd68c379b52fe7bc931175d2a2e1c5ee
ptoooool <55444828+ptoooool@users.noreply.github.com>
2020-06-24 14:27:00 +0900

    Start Project

---
+++ Web_Influencer/src/main/java/com/influencer/demo/WebInfluencerApplication.java
@@ -0,0 +1,13 @@
+package com.influencer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInfluencerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebInfluencerApplication.class, args);
+	}
+
+}
